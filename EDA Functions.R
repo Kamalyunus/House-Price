32974feadccaef52b65df2e63a1d730d2aa7830c
kamalyunus <yunus.kamal@gmail.com>
2017-03-05 13:36:28 -0600

    Add Corr Plots

--- EDA Functions.R
+++ EDA Functions.R
@@ -310,3 +310,19 @@ DF_Type<-function(df,type){
 }
   
 
+#Compute P-value of correlations
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+
